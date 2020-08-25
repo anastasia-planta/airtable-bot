d87b0997efd7efa819827fafd49958ec1e0841ec
anastasia-planta <69844964+anastasia-planta@users.noreply.github.com>
2020-08-25 18:01:52 +0300

    fifth commit to Github

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-TOKEN = os.environ["TOKEN"]
+TOKEN = os.environ.get('TOKEN', None)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
